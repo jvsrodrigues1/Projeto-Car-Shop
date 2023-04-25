24f283f423c345251de8240137317d4e1dfeb1c7
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-04-25 15:36:17 -0300

    Adiciona Car Controller.ts

---
+++ src/Controllers/CarControllers.ts
@@ -0,0 +1,73 @@
+import { NextFunction, Request, Response } from 'express';
+import ICar from '../Interfaces/ICars';
+import CarService from '../Services/CarService';
+import CarODM from '../Models/CarModel';
+
+export default class CarController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private service: CarService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new CarService(new CarODM());
+  }
+
+  public async register() {
+    const car: ICar = {
+      model: this.req.body.model,
+      year: this.req.body.year,
+      color: this.req.body.color,
+      status: this.req.body.status || false,
+      buyValue: this.req.body.buyValue,
+      doorsQty: this.req.body.doorsQty,
+      seatsQty: this.req.body.seatsQty,
+    };
+
+    try {
+      const newCar = await this.service.register(car);
+      return this.res.status(201).json(newCar);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async getAll(): Promise<Response> {
+    const cars = await this.service.getAll();
+    return this.res.status(200).json(cars);
+  }
+
+  public async getById() {
+    try {
+      const { id } = this.req.params;
+      const car = await this.service.getById(id);
+      return this.res.status(200).json(car);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async updateById() {
+    try {
+      const { id } = this.req.params;
+      const car = this.req.body;
+      const updatedCar = await this.service.updateById(id, car);
+      return this.res.status(200).json(updatedCar);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async deleteById() {
+    try {
+      const { id } = this.req.params;
+      await this.service.deleteById(id);
+      return this.res.status(204).end();
+    } catch (error) {
+      this.next(error);
+    }
+  }
+}
