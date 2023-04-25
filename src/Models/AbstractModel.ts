9c22d49f088fef4d9789153615ba3fb18dd4e62f
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-04-25 15:10:56 -0300

    Iniciando o projeto criacao da model Abstract e refatoramento da server.ts

---
+++ src/Models/AbstractModel.ts
@@ -0,0 +1,36 @@
+import { Model, Schema, model, models } from 'mongoose';
+
+export default abstract class AbstractModel<T> {
+  readonly model: Model<T>;
+  private schema: Schema<T>;
+
+  constructor(schema: Schema<T>, modelName: string) {
+    this.schema = schema;
+    this.model = models[modelName] || model(modelName, this.schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+
+  public async findAll(): Promise<T[] | null> {
+    return this.model.find({});
+  }
+
+  public async findById(id: string): Promise<T | null> {
+    return this.model.findById(id);
+  }
+
+  public async updateById(id: string, obj: Partial<T>):
+  Promise<T | null> {
+    return this.model.findByIdAndUpdate(
+      { _id: id },
+      obj,
+      { new: true },
+    );
+  }
+
+  public async deleteById(id: string): Promise<T | null> {
+    return this.model.findByIdAndDelete(id);
+  }
+}
