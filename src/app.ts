345807daa1a1dcd3238d69cb8e4b0d307d8eab2b
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-04-25 15:44:45 -0300

    Refatoramento App.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import routes from './Routes/Route';
+import ErrorHandler from './Middlewares/HandleErros';
 
 const app = express();
+
+app.use(routes);
+app.use(ErrorHandler.handle);
 
 export default app;
