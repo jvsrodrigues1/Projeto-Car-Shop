0d9aa525fb3061ee48593f954799c5fb56909777
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-04-28 20:06:51 -0300

    Refatoramento App.js

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import ErrorH from './Middlewares/HandleErrors';
-import routes from './Routes/Routes';
+import carRoutes from './Routes/CarRoutes';
 // push das estrelas!!!
 
 const app = express();
 
 app.use(express.json());
 
-app.use(routes);
+app.use('/', carRoutes);
 app.use(ErrorH.handle);
 
 export default app;
